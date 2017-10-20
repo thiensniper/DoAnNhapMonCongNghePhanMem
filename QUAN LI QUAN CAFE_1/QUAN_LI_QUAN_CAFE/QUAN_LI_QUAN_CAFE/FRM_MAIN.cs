d332a881026099cd864dc1f8aa9220b603fcba50
Thiện Thần Thánh <nguyenminhthiensmart@gmail.com>
2017-10-20 15:55:19 +0700

    Revert "Đã đổi frm_Main"
    This reverts commit cec516cf1e5cf2a33ba5538e43769beef05cd973.

--- QUAN LI QUAN CAFE_1/QUAN_LI_QUAN_CAFE/QUAN_LI_QUAN_CAFE/FRM_MAIN.cs
+++ QUAN LI QUAN CAFE_1/QUAN_LI_QUAN_CAFE/QUAN_LI_QUAN_CAFE/FRM_MAIN.cs
@@ -12,7 +12,7 @@ namespace QUAN_LI_QUAN_CAFE
 {
     public partial class FRM_MAIN : Form
     {
-        public FRM_MAIN()//Đổi gì đó
+        public FRM_MAIN()
         {
             InitializeComponent();
         }
